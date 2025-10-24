ee3d5ecc0bee16cbaaa4dfc3ef8aca956f8c6682
subhajitlucky <subhajitpradhan310@gmail.com>
2025-10-24 06:41:34 +0000

    Fix theme check execution in useSpaceTheme hook

--- src/hooks/useSpaceTheme.js
+++ src/hooks/useSpaceTheme.js
@@ -24,6 +24,7 @@ function useSpaceTheme() {
     };
 
     if (typeof MutationObserver === 'undefined') {
+      checkTheme();
       return undefined;
     }
 
@@ -39,6 +40,8 @@ function useSpaceTheme() {
       attributes: true,
       attributeFilter: ['data-theme']
     });
+
+    checkTheme();
 
     return () => observer.disconnect();
   }, []);
